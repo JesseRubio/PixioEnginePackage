13240f32a62903c8b439e352419cd8d1dde05fc5
Jess <jrubio87@hotmail.com>
2024-08-16 15:13:19 -0500

    Add files via upload

---
+++ Tests/PixioEnginePackageTests/PixioEnginePackageTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import PixioEnginePackage
+
+    final class PixioEnginePackageTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(PixioEnginePackage().text, "Hello, World!")
+        }
+    }
